38c0ecbefe269e385ae56bdd47708172d061085c
magvas <info@magvas>
2018-02-22 22:06:27 +0100

    Added screenshot functionality

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
-var videoTrack;
+var videoTrack = null;
+var videoOutput = null;
+var canvas = null;
+var ctx = null;
 
 function init(){
     navigator.getUserMedia =    navigator.getUserMedia || 
                                 navigator.webkitGetUserMedia ||
                                 navigator.mozGetUserMedia || 
                                 navigator.msGetUserMedia;
+    
+    videoOutput = document.getElementById("videoOutput");
+    videoOutput.addEventListener("click",capture,false);    
+    canvas = document.getElementById('canvasOutput');
+    ctx = canvas.getContext('2d');
+
 }
 
 var errorCallback = function(e) {
@@ -12,11 +21,7 @@ var errorCallback = function(e) {
   };
 
 
-function startCamera(){
-    
-    
-    var videoOutput = document.getElementById("videoOutput");
-
+function startCamera(){    
     if (navigator.getUserMedia) {
         navigator.getUserMedia({audio:false, video:true}, (stream) => {
             videoOutput.src = window.URL.createObjectURL(stream);
@@ -28,4 +33,14 @@ function startCamera(){
 
 function stopCamera() {
     videoTrack.stop();
+}
+
+function capture() {
+    if (videoTrack) {
+        ctx.drawImage(videoOutput, 0, 0);
+        // "image/webp" works in Chrome.
+        // Other browsers will fall back to image/png.
+        document.getElementById('imgOutput').src = canvas.toDataURL('image/webp');
+  
+    }
 }
